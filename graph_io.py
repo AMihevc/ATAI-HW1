fb9c1a0ad5f5df82b02766fdf4613a96a8ded650
AMihevc <mihevc.anze@gmail.com>
2023-09-25 16:06:50 +0100

    Saving and reading from file. Cleanup of the code

--- graph_io.py
+++ graph_io.py
@@ -3,10 +3,11 @@ import time
 import random
 import gzip
 import sys
+import igraph
 from hw1_prof_solution import connected_components
 
-#increase recursion limit for our method to work properly with large graphs
-sys.setrecursionlimit(10001)
+# increase recursion limit for our method to work properly with large graphs
+sys.setrecursionlimit(100001)
 
 # Manuel's recursive solution
 def connected_components_alternative1(edges):
@@ -37,7 +38,18 @@ def connected_components_alternative1(edges):
 
     return len(components)
 
-#Professor's function to crate a set of random graphs
+
+def connected_components_alternative2(edges):
+
+    graph = igraph.Graph.TupleList(edges)
+
+    components = graph.connected_components()
+    # here should we use the connected_components() method since clusters is deprecated
+
+    return len(components)
+
+
+# Professor's function to crate a set of random graphs
 def mk_instance(n, k, r):
     """Set random seed to 'r' and create a graph withn 'n' vertices and (up to) n * k edges."""
     random.seed(r)
@@ -51,7 +63,7 @@ def mk_instance(n, k, r):
             E.add((i,j))
     return V, E
 
-#Professor's function to write a graph to a file
+# Professor's function to write a graph to a file
 def write_instance(V, E, filename):
     """Write a graph instance to a file (loosely, in a format used in DIMACS challenges)."""
     with open(filename, "w") as f:
@@ -60,7 +72,7 @@ def write_instance(V, E, filename):
         for (i,j) in E:
             f.write(f"E {i} {j}\n")
 
-#Professor's function to read a graph from a file
+# Professor's function to read a graph from a file
 def read_instance(filename):
     """Read a graph from a file."""
     try:
@@ -95,37 +107,30 @@ def read_instance(filename):
 
 if __name__ == "__main__":
     
-    #time keeping
+    # time keeping
     total_time_prof = 0
     total_time_own = 0
+    total_time_alt2 = 0
+    flag = False
 
-    #creating and testing graphs
-
-    #for n in [100, 1000, 10000, 100000, 1000000]: # old for loop (too big for my computer)
-    for n in [100, 1000, 10000]: #number of vertices
+    # for n in [100, 1000, 10000, 100000, 1000000]: # old for loop (too big for my computer)
+    for n in [100, 1000, 10000, 100000]: #number of vertices
         for k in [1, 2, 5, 10]: #n*k = max number of edges
             for r in range(1,11): #r is the random seed
+                filename = f"G{n}-{k}-{r}.graph"
                 
                 # # uncomment for creating files with graphs (may be rather large):
                 # V, E = mk_instance(n, k, r)
-                # filename = f"G{n}-{k}-{r}.graph"
                 # write_instance(V, E, filename)
-                # print(n, k, len(E), connected_components(V, E))
 
 
-                # # uncomment for reading graphs from files and solving with 'default' method:
-                # filename = f"G{n}-{k}-{r}.graph"
+                # # uncomment for reading graphs from files method: 
                 # V, E = read_instance(filename)
-                # start = time.process_time()
-                # ncomponents = connected_components(V,E)
-                # end = time.process_time()
-                # cpu = end - start
-                # print(f"{filename}: {ncomponents} components, {cpu} seconds")
                 
-                # uncomment for creating benchmark set in memory and solving with 'default' method:
-                V, E = mk_instance(n, k, r)
+                # # uncomment for creating benchmark set in memory method:
+                # V, E = mk_instance(n, k, r)
                 
-                # ncomponents = connected_components(V,E) #this is the original line but it doesn't work with the "old" solution (too many arguments)
+                # profs method
                 start_prof = time.process_time()
                 ncomponents_prof = connected_components(E) # defult method provided by the professor
                 end_prof = time.process_time()
@@ -135,26 +140,58 @@ if __name__ == "__main__":
                 ncomponents_own = connected_components_alternative1(E) # our own recursive method
                 end = time.process_time()
                 
-                #check if the result of the 2 methods is equal
+                # only time our own method
+                start_alt2 = time.process_time()
+                ncomponents_alt2 = connected_components_alternative2(E) # our own recursive method
+                end_alt2 = time.process_time()
+
+                # check if the result of the alt1 and default method are the same
                 if ncomponents_prof != ncomponents_own:
-                    print(f"ERROR: number of components differs! at {n}, {k}, {r}")
+                    print(f"ERROR: number of components differs alt1 at {n}, {k}, {r}")
+                    flag = True
                     continue
                 
-                #add the time to the total time
+                # check if the result of the alt2 and default method are the same
+                if ncomponents_prof != ncomponents_own:
+                    print(f"ERROR: number of components differs alt2 at {n}, {k}, {r}")
+                    flag = True
+                    continue
+
+                # add the time to the total time prof 
                 cpu_prof = end_prof - start_prof
                 total_time_prof += cpu_prof
 
-                #add the time to the total time
+                # add the time to the total time alt1 
                 cpu = end - start
                 total_time_own += cpu
 
-                #output of results
-                filename = f"G{n}-{k}-{r} from memory"
-                print(f"{filename}: {ncomponents_own} components, {cpu} seconds")
+                # add the time to the total time alt2
+                cpu_alt2 = end_alt2 - start_alt2
+                total_time_alt2 += cpu_alt2
+
+                # # output of results
+                # print(f"{filename}: {ncomponents_own} components, {cpu} seconds")
+                
+                # # instead of printing this to the terminal, we write it to a file
+                with open("results.txt", "a") as f:
+                    f.write(f"{filename}: {ncomponents_own} components, {cpu} seconds\n")
+
     
-    #output of total time
+    # catching the mismatch
+    if flag: 
+        print("ERROR: There was a mismatch in number of components between the default and our method.")
+    
+    # output of total time
     print(f"Total time for the professor's method: {total_time_prof} seconds")
     print(f"Total time for our own method: {total_time_own} seconds")
+    print(f"Total time for our alt2 method: {total_time_alt2} seconds")
+
+    # also write the total time to the file
+    with open("results.txt", "a") as f:
+        f.write(f"Total time for the professor's method: {total_time_prof} seconds\n")
+        f.write(f"Total time for our own method: {total_time_own} seconds\n")
+        f.write(f"Total time for our alt2 method: {total_time_alt2} seconds\n")
+
 
     # # uncomment for reading a graphs from a particular file and solving with 'default' method
     # if len(sys.argv) != 2:
